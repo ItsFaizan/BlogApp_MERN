f82c1e852020796525e9bb622562fea7f9c3d354
ItsFaizan <faizanjavid71@gmail.com>
2023-08-13 05:42:06 +0500

    Login Form Created

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -4,7 +4,7 @@ import { verifyToken } from '../middleware/jwt.js';
 const authRoute = express.Router();
 
 authRoute.post("/user_register", register)
-authRoute.post("/user_login",verifyToken, login)
+authRoute.post("/user_login", login)
 authRoute.post("/user_logout",verifyToken, logout)
 
 export default authRoute;
